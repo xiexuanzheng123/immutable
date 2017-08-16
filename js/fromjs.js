6f9bd81d8697cc1c90ebaedbc7cdece3583966fc
xiexuanzheng123 <349328984@qq.com>
2017-08-16 16:56:57 +0800

    add an example of fromjs

--- js/fromjs.js
+++ js/fromjs.js
@@ -1,22 +1,30 @@
 import Immutable from 'immutable';
 
-
 /**
  * 这两种方法都支持数据的嵌套
  * 
  */
 
-/**Convert array to List */
-console.log(Immutable.fromJS([1, 2]));
+console.log(Immutable.fromJS([1, 2]));/**Convert array to List */
 
-/**Convert object to Map */
-console.log(Immutable.fromJS({a: 1}));
+console.log(Immutable.fromJS({a: 1}));/**Convert object to Map */
 
 
 /*
-*以下两种方法不支持数据嵌套
+* 以下两种方法不支持数据嵌套
+*
 */
 console.log(Immutable.List([1, 2]));
 console.log(Immutable.Map({a: 1}));
 
+Immutable.fromJS({a: {b: [10, 20, 30]}, c: 40}, function(key, value) {
+    console.log(key,value);
+    let isIndexed = Immutable.Iterable.isIndexed(value);
+    return isIndexed ? value.toList() : value.toOrderedMap();
+});
 
+// Immutable.fromJS({ a: {b: [10, 20, 30]}, c: 40}, function (key, value, path) {
+//   console.log(key, value, path);
+//   var isIndexed = Immutable.Iterable.isIndexed(value);
+//   return isIndexed(value) ? value.toList() : value.toOrderedMap()
+// });
